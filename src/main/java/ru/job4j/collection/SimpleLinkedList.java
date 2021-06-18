c1de31561431b1ee2d45e8149aec99c3e2dc633c
Roman Semykin <KindPanic@mail.ru>
2021-06-18 17:44:44 +0300

    2. Создать контейнер на базе связанного списка [#159 #168821]

--- src/main/java/ru/job4j/collection/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -1,6 +1,8 @@
 package ru.job4j.collection;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SimpleLinkedList<E> implements List<E> {
@@ -13,22 +15,6 @@ public class SimpleLinkedList<E> implements List<E> {
     public SimpleLinkedList() {
         this.lastNode = new Node<E>(null, firstNode, null);
         this.firstNode = new Node<E>(null, null, lastNode);
-    }
-
-    public int getModCount() {
-        return modCount;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public Node<E> getFirstNode() {
-        return firstNode;
-    }
-
-    public Node<E> getLastNode() {
-        return lastNode;
     }
 
     @Override
@@ -53,6 +39,29 @@ public class SimpleLinkedList<E> implements List<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return new SimpleLinkedIterator<>(this);
+        return new Iterator<E>() {
+
+            private final int exceptedModCount = modCount;
+            int cursor = 0;
+            private Node<E> rsl = firstNode;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size && modCount != 0;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (exceptedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                cursor++;
+                rsl = rsl.getNextElement();
+                return rsl.getCurrentElement();
+            }
+        };
     }
 }
