6470b2f963a00b008a9e0d5a6f8f75232df35f74
Roman Semykin <KindPanic@mail.ru>
2021-10-10 12:32:29 +0300

    0. Что такое Socket? [#4850 #213921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -4,13 +4,9 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Arrays;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -18,35 +14,20 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String str = in.readLine();
-                    while (in.ready()) {
+                    while (in.ready()){
+                        String str = in.readLine();
                         if (Arrays.stream(str.split(" "))
                                 .flatMap(p -> Arrays.stream(p.split("=")))
-                                .anyMatch("Exit"::equals)) {
-                            out.write(("Server was closed" + System.lineSeparator()).getBytes());
+                                .anyMatch("Bye"::equals)) {
+                            socket.close();
                             server.close();
                             break;
                         }
-                        if (Arrays.stream(str.split(" "))
-                                .flatMap(p -> Arrays.stream(p.split("=")))
-                                .anyMatch("Hello"::equals)) {
-                            out.write(("Hello, dear friend." + System.lineSeparator()).getBytes());
-                            break;
-                        }
-                        if (Arrays.stream(str.split(" "))
-                                .flatMap(p -> Arrays.stream(p.split("=")))
-                                .findFirst().isPresent()) {
-                            out.write(("What." + System.lineSeparator()).getBytes());
-                            break;
-                        }
+                        System.out.println(str);
                     }
                     out.flush();
                 }
             }
-        } catch (Exception e) {
-            LOG.error("Exception in log example", e);
         }
     }
-
-
 }
