10d7c947ed7348515b5c913848e0224d0f6d82ab
Roman Semykin <KindPanic@mail.ru>
2021-10-24 21:38:17 +0300

    0. Select [#6861]

---
+++ scripts/animals_db/insertInFauna.sql
@@ -0,0 +1,5 @@
+insert into fauna(name, avg_age, discovery_date) values('fisherman', '11111', date '2000.01.25');
+insert into fauna(name, avg_age) values('kraken', '9999');
+insert into fauna(name, avg_age) values('tashirfish', '20000');
+insert into fauna(name, avg_age, discovery_date) values('hobot', '1000', date '1500.08.17');
+insert into fauna(name, avg_age, discovery_date) values('hobobob', '21111', date '100.09.10');
---
+++ scripts/animals_db/selectAvgAge.sql
@@ -0,0 +1,1 @@
+select * from fauna where avg_age > 10000 and avg_age < 21000;
